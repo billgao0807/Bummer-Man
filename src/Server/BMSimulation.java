114808ede6354350e810f6dd4de76eba51f37d14
Lazy-Y <zhenyanz@usc.edu>
2015-11-13 21:28:23 -0800

    load board

--- src/Server/BMSimulation.java
+++ src/Server/BMSimulation.java
@@ -1,10 +1,16 @@
 package Server;
 
+import java.awt.Point;
 import java.util.Timer;
 import java.util.Vector;
 
+import Utilities.BMLibrary;
+import Utilities.BMNodeType;
+import Utilities.BMResult;
+import node.BMNode;
+
 public class BMSimulation extends Thread {
-	private BMNode[][] nodes;
+	private BMNode[][] board;
 	private Vector<BMPlayer> players;
 	private BMPlayer host;
 	private int timeLeft;
@@ -12,17 +18,53 @@ public class BMSimulation extends Thread {
 	private int totalHP;
 	public BMSimulation(BMPlayer host, int port){
 		BMHostServer hs = new BMHostServer(port);
+		loadBoard(BMLibrary.loadBoard());
 	}
+	private void loadBoard(int[][] board){
+		for (int i = 0; i < 16; i++){
+			for (int j = 0; j < 16; j++){
+				this.board[i][j] = initNode(i,j,board[i][j]);
+			}
+		}
+	}
+	private BMNode initNode(int i, int j, int id){
+		if (id > 4){
+			return new BMNodeItem(i,j,board,id);
+		}
+		else if (id == BMNodeType.bombing){
+			return new BMBombing(i,j,board);
+		}
+		else if (id == BMNodeType.bomb){
+			return new BMBomb(i,j,board);
+		}
+		else if (id == BMNodeType.tile){
+			return new BMTile(i,j,board);
+		}
+		else if (id == BMNodeType.wall){
+			return new BMWall(i,j,board);
+		}
+		else return new BMRoad(i,j,board);
+	}
+	
+	
 	public void startTimer(){
 		new Thread(new Runnable(){
 			@Override
 			public void run() {
-				while (timeLeft > 0){
-					Thread.sleep(1000);
+				while (timeLeft >= 0){
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						System.out.println("Start Timer Exception: " + e.getMessage());
+					}
 					timeLeft--;
 				}
+				BMSimulation.this.endGame();
 			}			
 		}).start();
+	}
+	protected void endGame() {
+		
 	}
 	public void setVariables(int time, int HP){
 		timeLeft = time;
@@ -31,11 +73,29 @@ public class BMSimulation extends Thread {
 	public Vector<String> getHPs(){
 		Vector<String> HPs = new Vector<String>();
 		for (BMPlayer player : players){
-			String hp = player.getHP() + "/" + totalHP;
+			String hp = player.getHP();
 			HPs.add(hp);
 		}
 		return HPs;
 	}
+	public Vector<BMResult> getResult(){
+		Vector<BMResult> results = new Vector<BMResult>();
+		for (BMPlayer player : players){
+			BMResult result = player.getResult();
+			results.add(result);
+		}
+		return results;
+	}
+	public boolean canMove(int x, int y){
+		return board[x][y].canMove();
+	}
+	public BMNode getNode(int x, int y){
+		return board[x][y];
+	}
+	public void dropBomb(int x, int y, BMPlayer player){
+		board[x][y] = new BMBomb(x,y,board,player.power,player.detonatedTime);
+	}
+	public void setNode()
 //	 In charge of all the information of one round of the game. 
 //		Function:
 //			+ BMSimulation(BMPlayer host, String ip)
