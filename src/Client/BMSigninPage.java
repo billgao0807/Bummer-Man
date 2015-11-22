a784c53827a4f1f14b5e684f83b29f7f3dace0c9
Jiahui Wei <jiahuiwe@usc.edu>
2015-11-22 15:18:04 -0800

    no message

--- src/Client/BMSigninPage.java
+++ src/Client/BMSigninPage.java
@@ -1,143 +1,127 @@
 package Client;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 import Utilities.BMFontLibrary;
-import Utilities.BMLibrary;
-import customUI.ClearPanel;
-import customUI.PaintedButton;
-import customUI.PaintedPanel;
 
+import javax.swing.SwingConstants;
 
 public class BMSigninPage extends JFrame{
-private static final long serialVersionUID = 5147395078473323173L;
-	
+	private static final long serialVersionUID = 5147395078473323173L;
+
 	private final static Dimension minSize = new Dimension(320,480);
-//	private final static Dimension maxSize = new Dimension(960,640);
-	public JTextField nameInput;
-	public JTextField passwordInput;
-	
+		private final static Dimension maxSize = new Dimension(960,640);
+	public JTextField txtUsername;
+	public JTextField txtPassword;
+
+
 	public BMSigninPage(ActionListener signup, ActionListener quickG, ActionListener login)
 	{
 		setTitle("Bomberman Sign In");
 		setSize(minSize);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		signIn si = new signIn(signup, quickG, login, BMLibrary.readImages("frame0.png"));
-		add(si);
 		
-
-	}
-class signIn extends PaintedPanel
-{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private PaintedButton signupB;
-	private PaintedButton quickGameB;
-	private PaintedButton loginB;
-	signIn(ActionListener signup, ActionListener quickG, ActionListener login, Image image)
-	{
-		super(image, true);
-		setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		setVisible(true);
-		
-		nameInput = new JTextField();
-		nameInput.setText("Username: ");
-		nameInput.setForeground(Color.GRAY);
-		nameInput.setPreferredSize(new Dimension(100, 20));
-		nameInput.setFont(BMFontLibrary.getFont("font.ttf", Font.PLAIN, 13));
-		nameInput.addMouseListener(new MouseAdapter(){
+		txtUsername = new JTextField(SwingConstants.CENTER);
+		txtUsername.setFont(BMFontLibrary.getFont("font2.otf", Font.PLAIN, 20));
+		txtUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		txtUsername.setText("Username");
+		txtUsername.setColumns(10);
+		txtUsername.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
-                nameInput.setText("");
+                txtUsername.setText("");
             }
         });
-		JPanel userName = new JPanel();
-		userName.setOpaque(false);
-		userName.add(nameInput);
 		
-		passwordInput = new JTextField();
-		passwordInput.setPreferredSize(new Dimension(12,30));
-//		passwordInput.setFont(f);
-		passwordInput.setText("Password:");
-		passwordInput.setForeground(Color.GRAY);
-	//	passwordInput.setFont();
-		passwordInput.setFont(BMFontLibrary.getFont("font.ttf", Font.PLAIN, 13));
-
-		passwordInput.setPreferredSize(new Dimension(100, 20));
-		passwordInput.addMouseListener(new MouseAdapter(){
+		txtPassword = new JTextField(SwingConstants.CENTER);
+		txtPassword.setFont(BMFontLibrary.getFont("font2.otf", Font.PLAIN, 20));
+		txtPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		txtPassword.setText("Password");
+		txtPassword.setColumns(10);
+		txtPassword.addMouseListener(new MouseAdapter(){
             @Override
             public void mouseClicked(MouseEvent e){
-                passwordInput.setText("");
+                txtPassword.setText("");
             }
         });
-		JPanel password = new JPanel();
-		password.setLayout(new GridBagLayout());
-		GridBagConstraints gbc1 = new GridBagConstraints();
-		gbc1.gridx = 0;
-		gbc1.gridy = 0;
-		password.setOpaque(false);
-		password.add(passwordInput, gbc1);
 		
-		signupB = new PaintedButton(
-				"Start",
-				BMLibrary.readImages("button0.png"),
-				BMLibrary.readImages("button0-0.png"),
-				22
-				);
-		signupB.addActionListener(signup);
-		//signupB.setFont(BMFontLibrary.getFont("font.ttf", Font.PLAIN, 20));
-		JPanel signupP = new JPanel();
-		signupP.add(new ClearPanel());
-		signupP.add(signupB);
-		signupP.setOpaque(false);
-		quickGameB = new PaintedButton("Quick Game", BMLibrary.readImages("button0.png") , BMLibrary.readImages("button0-0.png"), 18);
-		quickGameB.addActionListener(quickG);
-		quickGameB.setFont(BMFontLibrary.getFont("font.ttf", Font.PLAIN, 20));
-		JPanel quickGame = new JPanel();
-		quickGame.setOpaque(false);
-		quickGame.add(quickGameB);
-		loginB = new PaintedButton("Log In", BMLibrary.readImages("button0.png") , BMLibrary.readImages("button0-0.png"), 18);
-		//loginB.addActionListener(
-			//	);
-		loginB.addActionListener(login);
-		loginB.setFont(BMFontLibrary.getFont("font.ttf", Font.PLAIN, 20));
-		JPanel loginP = new JPanel();
-		loginP.setOpaque(false);
-		loginP.add(loginB);
+		JButton btnQuickgame = new JButton("QuickGame");
+		btnQuickgame.addActionListener(quickG);
+		btnQuickgame.setFont(BMFontLibrary.getFont("font2.otf", Font.PLAIN, 20));
 		
-		gbc.gridy = 1;
-		add(userName,gbc);
-		gbc.gridy = 2;
-		add(password,gbc);
-		gbc.gridy = 3;
-		add(quickGame,gbc);
-		gbc.gridy = 4;
-		add(signupP,gbc);
-		gbc.gridy = 5;
-		add(loginP,gbc);
+		JButton btnSignup = new JButton("Signup");
+		btnSignup.addActionListener(signup);
+		btnSignup.setFont(BMFontLibrary.getFont("font2.otf", Font.PLAIN, 20));
+		btnSignup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
+		JButton btnLogin_1 = new JButton("Login");
+		btnLogin_1.addActionListener(login);
+		btnLogin_1.setFont(BMFontLibrary.getFont("font2.otf", Font.PLAIN, 20));
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(60)
+					.addComponent(btnQuickgame, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+					.addGap(60))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(105)
+							.addComponent(btnSignup, GroupLayout.PREFERRED_SIZE, 24, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(105)
+							.addComponent(btnLogin_1, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)))
+					.addGap(105))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(100)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(txtPassword, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+							.addGap(100))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(txtUsername, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+							.addGap(100))))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(69)
+					.addComponent(txtUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
+					.addComponent(btnQuickgame)
+					.addGap(18)
+					.addComponent(btnSignup)
+					.addGap(18)
+					.addComponent(btnLogin_1)
+					.addGap(67))
+		);
+		getContentPane().setLayout(groupLayout);
+
+
+
+		//		signIn si = new signIn(signup, quickG, login, BMLibrary.readImages("background4.png"));
+		//		add(si);
+
+
 	}
-}
-public void closeMe() {
-	this.setVisible(false);
-	
-}
 }
