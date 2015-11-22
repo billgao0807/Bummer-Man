46b7b8d53c00240b76e1deb009244a9d787aba49
Jiahui Wei <jiahuiwe@usc.edu>
2015-11-22 15:10:26 -0800

    no message

--- src/Client/BMResultFrame.java
+++ src/Client/BMResultFrame.java
@@ -5,8 +5,7 @@ import java.awt.Dimension;
 public class BMResultFrame {
 	public BMResultFrame()
 	{
-		final long serialVersionUID = 5147395078473323173L;
-		
+		final long serialVersionUID = 5147395078473323173L;		
 		final Dimension minSize = new Dimension(320,480);
 	}
 
